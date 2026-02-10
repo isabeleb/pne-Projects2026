f6c8f1192cf86d8f7b51c55dce9e6c6f0e28d725
isabeleb <i.escalante.2025@alumnos.urjc.es>
2026-02-10 16:06:43 +0100

    Answer to exercise 1 in session 3 uploaded

---
+++ S03/dna_count.py
@@ -0,0 +1,30 @@
+def count_bases(seq):
+    count_A = 0
+    count_C = 0
+    count_G = 0
+    count_T = 0
+    for base in seq:
+        if base == "A":
+            count_A += 1
+        elif base == "C":
+            count_C += 1
+        elif base == "G":
+            count_G += 1
+        elif base == "T":
+            count_T += 1
+    return count_A, count_C, count_G, count_T
+
+DNA_seq = (input("Enter a DNA sequence:")).upper()
+
+(count_A, count_C, count_G, count_T) = count_bases(DNA_seq)
+
+print("Length of the sequence:" , len(DNA_seq))
+
+print("A:", count_A)
+print("C:", count_C)
+print("G:", count_G)
+print("T:", count_T)
+
+
+
+
