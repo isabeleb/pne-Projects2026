b5040bcd60d22ed38a6ca3816c3b591e353c74b0
isabeleb <i.escalante.2025@alumnos.urjc.es>
2026-02-10 16:07:11 +0100

    Answer to exercise 2 in session 3 uploaded

--- S03/dna_count.py
+++ S03/dna_count.py
@@ -19,7 +19,6 @@ DNA_seq = (input("Enter a DNA sequence:")).upper()
 (count_A, count_C, count_G, count_T) = count_bases(DNA_seq)
 
 print("Length of the sequence:" , len(DNA_seq))
-
 print("A:", count_A)
 print("C:", count_C)
 print("G:", count_G)
