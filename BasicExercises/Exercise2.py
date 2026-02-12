5d4ef3203c870b94e9cb9d569dd46c4810a7d6cf
isabeleb <i.escalante.2025@>
2026-02-12 16:16:36 +0100

    Exercise 2 uploaded

---
+++ BasicExercises/Exercise2.py
@@ -0,0 +1,16 @@
+#The string with leading and trailing spaces removed (use strip())
+#The number of words in the string (use split())
+#The string with the first letter of each word capitalized (use title())
+#Whether the stripped string starts with "Hello" (use startswith())
+#Whether the stripped string ends with "ing." (use endswith())
+#The position (index) of the word "Python" in the stripped string(use find())
+#The words joined back together separated by " - " (use split() and " - ".join())
+
+text = "  Hello, World! Welcome to Python Programming.  "
+
+print("Text with no leading/trailing spaces:" , text.strip(" "))
+
+words_list = text.split()
+print("Word count:" , len(words_list))
+
+print("Capitalized:" , text.title())
