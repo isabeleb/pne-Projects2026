0d8582c422b2fd49d30affd2c01fa7ead9c8dd6b
isabeleb <i.escalante.2025@alumnos.urjc.es>
2026-02-16 23:46:30 +0100

    All basic exercises uploaded!

--- BasicExercises/Exercise8.py
+++ BasicExercises/Exercise8.py
@@ -14,11 +14,29 @@ students = [
 #and status. At the end, print how many students passed and how many failed.
 
 def average(grades):
-    grades = []
     grades_sum = 0
     for grade in grades:
         grades_sum += grade
-    average = grades_sum / len(grades)
-    return average
+    return grades_sum / len(grades)
 
-for
+def get_status(avg):
+    if avg >= 5:
+        return "PASS"
+    else:
+        return "FAIL"
+pass_count = 0
+fail_count = 0
+for student in students:
+    grades = student["grades"]
+    avg = average(grades)
+    status = get_status(avg)
+
+    print(student['name'] , ":" , round(avg , 1) , "->" , get_status(avg) )
+
+    if status == "FAIL":
+        fail_count += 1
+    else:
+        pass_count += 1
+
+
+print("\nResults:" , pass_count , "passed ," , fail_count , "failed")
