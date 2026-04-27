76857a5130c8108b14cee0d3d0ae4d63ff802c02
isabeleb <i.escalante.2025@alumnos.urjc.es>
2026-04-28 00:39:26 +0200

    Exercise 1 fro practice 7 uploaded!

---
+++ P07/e1.py
@@ -0,0 +1,25 @@
+import http.client
+import json
+
+SERVER = 'rest.ensembl.org'
+ENDPOINT = '/info/ping'
+PARAMS = '?content-type=application/json'
+URL = SERVER + ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+connect = http.client.HTTPConnection(SERVER)
+connect.request("GET", ENDPOINT + PARAMS)
+
+response = connect.getresponse()
+data = json.loads(response.read().decode())
+
+print(f"Response received!: {response.status} {response.reason}\n")
+
+if data["ping"] == 1:
+    print("PING OK! The database is running|")
+
+
+
