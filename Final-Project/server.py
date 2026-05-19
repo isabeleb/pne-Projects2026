464343c761d6389d1478ef0340a99ed894ce3d10
isabeleb <i.escalante.2025@alumnos.urjc.es>
2026-05-19 16:13:46 +0200

    Exercise 1 fro practice 7 uploaded!

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -1,59 +1,152 @@
 import http.server
+import http.client
 import socketserver
-import termcolor
+import json
 from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+import jinja2 as j
 
 PORT = 8080
-
 socketserver.TCPServer.allow_reuse_address = True
 
+SERVER = 'rest.ensembl.org'
+params = '?content-type=application/json'
+
 class TestHandler(http.server.BaseHTTPRequestHandler):
-
-
     def do_GET(self):
+        contents = ""
 
-        termcolor.cprint(self.requestline, 'green')
+        url_path = urlparse(self.path)
+        path = url_path.path
+        arguments = parse_qs(url_path.query)
 
-        request = self.requestline.strip("GET").strip("HTTP/1.1").strip(" ")
-        req_file = request.strip("/")
+        def read_html_file(filename):
+            contents = Path("html/" + filename).read_text()
+            contents = j.Template(contents)
+            return contents
 
-        try:
-            if request == "/" or request == "/index.html":
-                req_file = "index.html"
-                if req_file == "index.html":
-                    file_path = Path("html/" + req_file)
-                else:
-                    file_path = Path("html/" + req_file)
-            else:
-                file_path = Path("html/info/" + req_file.strip("info"))
-
-            contents = file_path.read_text()
-
+        if path == "/" or path == "/index.html":
             self.send_response(200)
+            contents = Path("html/index.html").read_text()
 
-        except (FileNotFoundError, IsADirectoryError, UnboundLocalError):
-            file_path = Path("html/" + "error.html")
-            contents = file_path.read_text()
-            self.send_response(404)
+
+        elif path == "/listSpecies":
+            self.send_response(200)
+            arg_limit = arguments.get("limit", [None])[0]
+            if arg_limit is None or arg_limit == "":
+                limit = None
+            else:
+                limit = int(arg_limit)
+
+            endpoint1 = '/info/species'
+            URL = SERVER + endpoint1 + params
+
+            print()
+            print(f"URL: {URL}")
+
+            connect = http.client.HTTPConnection(SERVER)
+
+            try:
+                connect.request("GET", endpoint1 + params)
+                response = connect.getresponse()
+                data = json.loads(response.read().decode())
+
+                all_species = data["species"]
+
+                total_species = len(all_species)
+
+                species_list = []
+                for sp in all_species:
+                    species_list.append(sp["display_name"])
+
+                if limit is None:
+                    final_list = species_list
+                else:
+                    final_list =species_list[:limit]
+
+                contents = read_html_file("SpeciesList.html").render(limit=limit, species_list=final_list, total_species=total_species)
+
+            except ConnectionRefusedError:
+                print('ERROR! Cannot connect to the server')
+                exit()
+
+
+        elif path == "/karyotype":
+            self.send_response(200)
+            species = arguments.get("species", ["human"])[0]
+
+            endpoint2 = f"/info/assembly/{species}"
+            URL = SERVER + endpoint2 + params
+
+            print()
+            print(f"URL: {URL}")
+
+            connect = http.client.HTTPConnection(SERVER)
+
+            try:
+                connect.request("GET", endpoint2 + params)
+                response = connect.getresponse()
+                data = json.loads(response.read().decode())
+
+                regions = data["top_level_region"]
+                karyotype = []
+                for region in regions:
+                    if region["coord_system"] == "chromosome":
+                        karyotype.append(region["name"])
+
+                contents = read_html_file("InfoKaryotype.html").render(species=species, karyo_info=karyotype)
+
+            except ConnectionRefusedError:
+                print('ERROR! Cannot connect to the server')
+                exit()
+
+
+        elif path == "/chromosomeLength":
+            self.send_response(200)
+            species = arguments.get("species", ["human"])[0]
+            chromosome = arguments.get("chromo", [""])[0]
+
+            endpoint3 = f"info/assembly/{species}"
+            URL = SERVER + endpoint3 + params
+
+            print()
+            print(f"URL: {URL}")
+
+            connect = http.client.HTTPConnection(SERVER)
+
+            try:
+                connect.request("GET", endpoint3 + params)
+                response = connect.getresponse()
+                data = json.loads(response.read().decode())
+
+                regions = data["top_level_region"]
+
+                chromosome_length = None
+
+                for region in regions:
+                    if str(region["name"]).strip(" ").lower() == chromosome:
+                        chromosome_length = region["length"]
+                        break
+                    else:
+                        chromosome_length = "Chromosome not found"
+
+                contents = read_html_file("ChromosomeLength.html").render(species=species, chromo=chromosome, chromo_length=chromosome_length)
+
+            except ConnectionRefusedError:
+                print('ERROR! Cannot connect to the server')
+                exit()
 
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(contents.encode()))
-
+        self.send_header('Content-Length', str(len(contents.encode())))
         self.end_headers()
-
         self.wfile.write(contents.encode())
 
 
 Handler = TestHandler
-
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-
     print("Serving at PORT", PORT)
-
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
-        print("")
-        print("Stopped by the user")
+        print("\nStopped by the user")
         httpd.server_close()
-
